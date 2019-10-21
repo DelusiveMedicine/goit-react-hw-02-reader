f6d3b23ee3bcec7af4b32902459dcffd6b2695f3
Pagu-Chan <enjoyorhate@gmail.com>
2019-10-21 13:13:15 +0300

    Updates

---
+++ precache-manifest.edc45d7b35b822086d1db9864df67a9b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a7a8fefe408f5136b8937ecc0eb08e27",
+    "url": "/goit-react-hw-02-reader/index.html"
+  },
+  {
+    "revision": "68eb2bc7c10da9719004",
+    "url": "/goit-react-hw-02-reader/static/css/main.696774e3.chunk.css"
+  },
+  {
+    "revision": "f421b5d7a19dc8f73f0c",
+    "url": "/goit-react-hw-02-reader/static/js/2.0aba6b90.chunk.js"
+  },
+  {
+    "revision": "68eb2bc7c10da9719004",
+    "url": "/goit-react-hw-02-reader/static/js/main.369ffd9d.chunk.js"
+  },
+  {
+    "revision": "58b57449e5428d7bb638",
+    "url": "/goit-react-hw-02-reader/static/js/runtime-main.4603fe05.js"
+  }
+]);
